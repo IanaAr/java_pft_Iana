731a47c2c9fbad43f054c9604387f2c00a4f764f
iana.melnik <sirrious@gmail.com>
2021-08-21 21:08:38 +0300

    add several functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,28 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram
-{
- public static void main (String[] args)
-  {
-  String hw = "Hello world!";
-  System.out.println(hw);
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    hw("world");
+    hw("user");
+    hw("Iana");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(l, a));
   }
 
+  public static void hw(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double l) {
+    return l * l;
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
