68d1545d2d2e2906790edcfe8e81302e022ee56b
iana.melnik <sirrious@gmail.com>
2021-09-06 00:44:10 +0300

    add location checking if to NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,18 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
-  private WebDriver wd;
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.tagName("h1")) &&
+            wd.findElement(By.tagName("h1")).getText().equals("Groups") &&
+            isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void goToHomePage() {
-    wd.get("http://localhost/addressbook/index.php");
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
   }
 
 }
