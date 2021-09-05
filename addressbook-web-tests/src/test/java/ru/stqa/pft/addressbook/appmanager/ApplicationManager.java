1fe6594429a27ccfaa94c8eccac7d0fc39ea9c26
iana.melnik <sirrious@gmail.com>
2021-09-05 15:29:32 +0300

    change == to  equals

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -22,9 +22,9 @@ public class ApplicationManager {
 
   public void init() {
 
-    if (browser == BrowserType.FIREFOX) {
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if ( browser==BrowserType.CHROME ) {
+    } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     } else {
       wd = new InternetExplorerDriver();
