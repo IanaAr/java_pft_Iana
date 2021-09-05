716303c08194d4071da1c004c4183c25f4771aff
iana.melnik <sirrious@gmail.com>
2021-09-05 16:16:31 +0300

    1. add new tests with null 2. add if to type

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,10 +6,37 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupCreationTests extends TestBase {
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreationAllFields() throws Exception {
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().intGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitCreation();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+  @Test
+  public void testGroupCreationTwoFields() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().intGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", null));
+    app.getGroupHelper().submitCreation();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+  @Test
+  public void testGroupCreationOneField() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().intGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
+    app.getGroupHelper().submitCreation();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+  @Test
+  public void testGroupCreationNoField() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().intGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData(null, null, null));
     app.getGroupHelper().submitCreation();
     app.getGroupHelper().returnToGroupPage();
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -15,4 +15,34 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
   }
 
+  @Test
+  public void testGroupModificationTwoFields() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("TEST1", "TEST2", null));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+  @Test
+  public void testGroupModificationOneField() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("TEST1", null, null));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+  @Test
+  public void testGroupModificationNoField() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData(null, null, null));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
 }
