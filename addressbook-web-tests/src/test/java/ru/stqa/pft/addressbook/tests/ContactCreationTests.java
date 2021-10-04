4f8d98d7102fa93e81c29a75ad98a8e069fe99d4
iana.melnik <sirrious@gmail.com>
2021-10-04 22:08:06 +0300

    parametrised creation tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,6 +6,9 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,49 +16,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-
-  @Test
-  public void testContactCreationAllFields() throws Exception {
-    app.navigationHelper().homePage();
-    Contacts before = app.contact().all();
-    ContactData contact = new ContactData()
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData()
             .withFirstname("TEST").withMiddlename("TEST")
             .withLastname("TEST").withCompany("TEST").withAddress("TEST")
-            .withHomePhone("2222").withMobilePhone("11111111111").withWorkPhone("11111")
-            .withFirstEmail("ww@ww.ww").withSecondEmail("www@w.w").withThirdEmail("ww@www.w")
-            .withAddress("testing street, testing home 4, testing appart. 11")
-            .withGroup("[none]");
-    app.contact().create(contact, true);
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
-    Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-    assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt())))
-    );
+            .withHomePhone("1111").withMobilePhone("11111111111").withWorkPhone("11111")
+            .withFirstEmail("ww1@ww.ww").withSecondEmail("www1@w.w").withThirdEmail("ww1@www.w")
+            .withAddress("testing street, testing home 1, testing appart.11")
+            .withPhoto(new File("src/test/resources/nature.png"))
+            .withGroup("[none]")});
+    list.add(new Object[]{new ContactData()
+            .withFirstname("TESTPhoto")
+            .withLastname("TESTPhoto")
+            .withPhoto(new File("src/test/resources/nature.png"))
+            .withGroup("[none]")});
+    list.add(new Object[]{new ContactData()
+            .withFirstname("TEST2Fields")
+            .withLastname("TEST2Fields")
+            .withGroup("[none]")});
+    return list.iterator();
   }
 
-  @Test
-  public void testContactCreationTwoFields() throws Exception {
+  @Test(dataProvider = "validContacts")
+  public void testContactCreationAllFields(ContactData contact) throws Exception {
     app.navigationHelper().homePage();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withFirstname("TEST1").withLastname("TEST1").withGroup("[none]");
-    app.contact().create(contact, true);
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
-    Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-    assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt())))
-    );
-  }
-
-  @Test
-  public void testContactCreationWithPhoto() throws Exception {
-    app.navigationHelper().homePage();
-    Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/nature.png");
-    ContactData contact = new ContactData()
-            .withFirstname("TESTPhoto").withLastname("TEST1")
-            .withGroup("[none]").withPhoto(photo);
     app.contact().create(contact, true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
