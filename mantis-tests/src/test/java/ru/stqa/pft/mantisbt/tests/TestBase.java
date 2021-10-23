ee39281c29f4451f1509974fbae67a67216c6442
iana.melnik <sirrious@gmail.com>
2021-10-23 20:00:40 +0300

    Задание №17: Подготовлен новый проект для нового учебного приложения
    Создан новый модуль в IDEA
    Добавлен в него конфигурационный файл Gradle и всё необходимое для запуска сборщика
    Создана структура директорий для хранения кода.
    Созданы TestBase и ApplicationManager (перенесены из существующего модуля и адаптированы, удален весь неиспользуемый код).

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantisbt/tests/TestBase.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.mantisbt.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantisbt.appmanager.ApplicationManager;
+
+
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+
+
+}
